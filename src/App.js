4cd51104727192fc445b604bdc31468744aafc26
Hector <hecrafalac@hotmail.com>
2023-03-28 19:20:52 -0600

    Botón de prueba

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './app.scss';
+import { Button } from 'carbon-components-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header aria-label="IBM Platform Name">
-          3    <HeaderName href="#" prefix="IBM">
-          4      [Platform]
-          5    </HeaderName>
-          6  </Header>
-      </header>
-    </div>
+      <Button>Button</Button>
   );
 }
 
