f9f1dd124a670fae3f366421bb169a0d0447f5e5
Hector <hecrafalac@hotmail.com>
2023-03-28 18:31:25 -0600

    hola xd

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header aria-label="IBM Platform Name">
+          3    <HeaderName href="#" prefix="IBM">
+          4      [Platform]
+          5    </HeaderName>
+          6  </Header>
       </header>
     </div>
   );
