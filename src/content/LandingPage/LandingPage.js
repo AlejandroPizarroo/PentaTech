201dda99712a895c2ff201915707df8783a7b1bd
AlanVilchis <83721974+AlanVilchis@users.noreply.github.com>
2023-04-11 17:01:56 -0600

    Test commit (Change LandingPage.js)

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const LandingPage = () => {
-    return <div>LANDING PAGE</div>;
+    return (
+        <div className="landing-page">
+            <h1>Welcome to my Landing Page</h1>
+            <p>Thanks for visiting my website. Here you will find all sorts of interesting information about me and my work.</p>
+            <button>Learn More</button>
+        </div>
+    );
 };
 
 export default LandingPage;
