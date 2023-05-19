2bcecde5b4652abdd72e566495f80047faba0e51
Alejandro Pizarro <alejandropizarro@alejandropizarro.svcs.itesm.mx>
2023-05-18 19:45:11 -0600

    front-end del login completado, no tiene funcionalidad

--- src/content/LoginPage/LoginPage.js
+++ src/content/LoginPage/LoginPage.js
@@ -1,84 +1,139 @@
-import React, {useState} from 'react';
-import {Header, HeaderName, Theme} from '@carbon/react';
-import {Grid, Column, TextInput, Button} from '@carbon/react';
-import {Link} from 'react-router-dom';
-import {ArrowRight} from '@carbon/react/icons';
+import React, { useState } from 'react';
+import { Header, HeaderName, Theme } from '@carbon/react';
+import { Grid, Column, TextInput, Button } from '@carbon/react';
+import { Link } from 'react-router-dom';
+import { ArrowRight } from '@carbon/react/icons';
 
 var formData = new URLSearchParams();
-////const emailRegex = /^[\w.%+-]+@ibm\.com$/i;
 const emailRegex = /^[\w.%+-]+@tec\.mx$/i;
+
 const HomePage = () => {
-    const [errorMessage, setErrorMessage] = useState("\u00A0\u00A0IBMid");
+    const [errorMessage, setErrorMessage] = useState('');
     const [inputValue, setInputValue] = useState('');
-    const[pageDir, setPageDir] = useState('/login');
+    const [pageDir, setPageDir] = useState('/login');
+    const [twoFACode, setTwoFACode] = useState('');
+    const [twoFAErrorMessage, setTwoFAErrorMessage] = useState('');
+    const [isRequestingTwoFA, setIsRequestingTwoFA] = useState(false);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
-        if (emailRegex.test(event.target.value)) {
-            formData.append('email', event.target.value) ;
-            setPageDir("/dashboard");
-        }
-        else {
+        const isValidEmail = emailRegex.test(event.target.value);
+        setErrorMessage(isValidEmail ? '' : 'Correo electrónico inválido');
+        if (isValidEmail) {
+            formData.append('email', event.target.value);
+            setPageDir('/dashboard');
+        } else {
             formData = new URLSearchParams();
-            setPageDir("/login");
+            setPageDir('/login');
         }
+    };
+
+    const handleTwoFACodeChange = (event) => {
+        const { value } = event.target;
+        setTwoFACode(value);
+        if (value.length === 0 || (value.length === 6 && /^\d+$/.test(value))) {
+            setTwoFAErrorMessage('');
+        } else {
+            setTwoFAErrorMessage('El código 2FA debe ser de 6 dígitos');
+        }
+    };
+
+    const handleRequestTwoFA = () => {
+        setIsRequestingTwoFA(true);
     };
 
     const handleButtonClick = () => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: formData
-        };
+        if (twoFACode.length === 6 && /^\d+$/.test(twoFACode)) {
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: formData
+            };
 
-        fetch('http://localhost:5000/api/auth/verifyEmail', requestOptions)
-            .then(response => response.json())
-            .then(_ =>{
-                setErrorMessage('');
-            })
-            .catch(error => {
-                console.error(error);
-                setErrorMessage("\u00A0\u00A0Se ingreso un email inválido");
-            });
-        formData = new URLSearchParams();
+            fetch('http://localhost:5000/api/auth/verifyEmail', requestOptions)
+                .then(response => response.json())
+                .then(_ => {
+                    setErrorMessage('');
+                    setTwoFAErrorMessage('');
+                })
+                .catch(error => {
+                    console.error(error);
+                    setErrorMessage('Se ingresó un correo electrónico inválido');
+                });
+            formData = new URLSearchParams();
+        } else {
+            setTwoFAErrorMessage('El código 2FA debe ser de 6 dígitos');
+        }
     };
 
-
-    return(
-    <body className="login-background">
-    <Theme theme="g100">
-        <Header aria-label="IBM Platform Name">
-            <HeaderName prefix="IBM">
-                Certifications Dashboard
-            </HeaderName>
-        </Header>
-    </Theme>
-    <Grid fullWidth condensed>
-        <Column lg={16} md={8} sm={4} className="upper-break"/>
-        <Column lg={16} md={8} sm={4}>
-            <Grid fullWidth condensed>
-                <Column lg={{span: 8, offset: 4}} md={{span: 4, offset: 2}} sm={{span: 2, offset: 1}}>
-                    <Theme theme="g100">
-                        <h3>&nbsp;Log in to the IBM Certifications Dashboard</h3>
-                        <TextInput
-                            id="text-input-1"
-                            type="email"
-                            labelText={errorMessage || "\u00A0\u00A0IBMid"}
-                            value={inputValue}
-                            onChange={handleInputChange}
-                            placeholder="username@ibm.com"
-                        />
-                        <Link to={pageDir}>
-                            <Button renderIcon={ArrowRight} onClick={handleButtonClick}>Log in</Button>
-                        </Link>
-                    </Theme>
-                </Column>
-            </Grid>
-        </Column>
-    </Grid>
-    </body>
+    return (
+        <body className="login-background">
+        <Theme theme="g100">
+            <Header aria-label="IBM Platform Name">
+                <HeaderName prefix="IBM">
+                    Certifications Dashboard
+                </HeaderName>
+            </Header>
+        </Theme>
+        <Grid fullWidth condensed>
+            <Column lg={16} md={8} sm={4} className="upper-break" />
+            <Column lg={16} md={8} sm={4}>
+                <Grid fullWidth condensed>
+                    <Column
+                        lg={{ span: 8, offset: 4 }}
+                        md={{ span: 4, offset: 2 }}
+                        sm={{ span: 2, offset: 1 }}
+                    >
+                        <Theme theme="g100">
+                            <h3>&nbsp;Log in to the IBM Certifications Dashboard</h3>
+                            <TextInput
+                                id="text-input-1"
+                                type="email"
+                                labelText={<>&nbsp;&nbsp;IBMid</>}
+                                value={inputValue}
+                                onChange={handleInputChange}
+                                placeholder="username@ibm.com"
+                                invalid={errorMessage !== ''}
+                                invalidText={errorMessage}
+                            />
+                            {pageDir === '/dashboard' && (
+                                <>
+                                    {isRequestingTwoFA ? (
+                                        <>
+                                            <Button enabled>
+                                                Request 2FA Code
+                                            </Button>
+                                            <div>&nbsp;</div>
+                                            <TextInput
+                                                id="text-input-2fa"
+                                                type="password"
+                                                labelText={<>&nbsp;&nbsp;2FA Code</>}
+                                                value={twoFACode}
+                                                onChange={handleTwoFACodeChange}
+                                                invalid={twoFAErrorMessage !== ''}
+                                                invalidText={twoFAErrorMessage}
+                                                maxLength={6}
+                                            />
+                                        </>
+                                    ) : (
+                                        <Button onClick={handleRequestTwoFA}>
+                                            Request 2FA Code
+                                        </Button>
+                                    )}
+                                </>
+                            )}
+                            <Link to={pageDir}>
+                                <Button renderIcon={ArrowRight} onClick={handleButtonClick} disabled={twoFACode.length !== 6}>
+                                    Log in
+                                </Button>
+                            </Link>
+                        </Theme>
+                    </Column>
+                </Grid>
+            </Column>
+        </Grid>
+        </body>
     );
-
-}
+};
 
 export default HomePage;
