425aaf04a7792f9fa97c8c199b32093e26fc3992
Fausto Palma <a01639224@tec.mx>
2023-05-22 20:16:17 -0600

    Home page changes

--- src/content/HomePage/HomePage.js
+++ src/content/HomePage/HomePage.js
@@ -1,65 +1,68 @@
 import React from 'react';
-import {Header, HeaderName, HeaderGlobalBar, OverflowMenu, OverflowMenuItem} from '@carbon/react';
-import {User} from '@carbon/react/icons';
-import {Content, Button} from '@carbon/react';
-import {Link} from 'react-router-dom';
-import ibmCertificationImg from './Home.jpg';
+import {Theme, Header, HeaderName, HeaderGlobalBar, OverflowMenu, OverflowMenuItem} from '@carbon/react';
+import {Content, Grid, Column, Button} from '@carbon/react';
+import {User, Upload, Filter, Search} from '@carbon/react/icons';
+import homeImage from './homePageImage.jpg';
 
 const HomePage = () => (
     <>
-        <Header aria-label="IBM Platform Name">
-            <HeaderName prefix="IBM">
-                Certifications Dashboard
-            </HeaderName>
-            <HeaderGlobalBar>
-                <OverflowMenu
-                    flipped="true"
-                    renderIcon={User}
-                    size="lg">
-                    <OverflowMenuItem itemText="Log in" href="/login"/>
-                </OverflowMenu>
-            </HeaderGlobalBar>
-        </Header>
+        <Theme theme="g100">
+            <Header aria-label="IBM Platform Name">
+                <HeaderName prefix="IBM">
+                    Certifications Dashboard
+                </HeaderName>
+                <HeaderGlobalBar>
+                    <OverflowMenu
+                        flipped="true"
+                        renderIcon={() => <User size={20}/>}
+                        size="lg">
+                        <OverflowMenuItem itemText="Log in" href="/login" className="overflow-menu-item"/>
+                    </OverflowMenu>
+                </HeaderGlobalBar>
+            </Header>
+        </Theme>
         <Content>
-            <h1 className="landing-page__heading">IBM Certifications Dashboard</h1>
-            <h2 className="landing-page__subheading">What is IBM CD?</h2>
-            <p className="landing-page__p">
-                IBM Certifications dashboard is a platform that allows IBM to compare
-                themselves against the rest of the industry. With filters, a search
-                function, and a functional dashboard to display data, the information
-                can be easily managed and compared so IBM can be the best in the industry.
-            </p>
-            <img
-                className="landing-page__img"
-                src={ibmCertificationImg}
-                alt="IBM Certification logo"
-            />
-            <Link to="/login">
-                <Button className="loginbutton">Log in page</Button>
-            </Link>
-            <h2 className="landing-page__subheadingFunc" id={"func1"}>The functionalities</h2>
-            <h2 className="landing-page__subheadingFunc">Upload Information</h2>
-            <p className="landing-page__p1">
-                IBM Certifications Dashboard allows you to save many Excels of the IBM employee's certifications.
-            </p>
-            <h2 className="landing-page__subheadingFunc">Search for an employee's information</h2>
-            <p className="landing-page__p1">
-                When searching for an employee with it's uid, the certifications of that employee as well as
-                the recommended certifications for him or her will be shown in a small window.
-            </p>
-            <h2 className="landing-page__subheadingFunc">Visualize Information</h2>
-            <p className="landing-page__p1">
-                Any uploaded information will be displayed in an esthetic and intuitive
-                way so that the data can be visually analyzed, therefore, when using either
-                the filters function the dashboard will update in real time.
-            </p>
-
-
+            <Grid className="banner">
+                <Column lg={8} md={4} sm={4} className="header">
+                    <h1>Your one-stop certifications hub</h1>
+                    <p className="header-body">
+                        The IBM Certifications Dashboard allows IBM to compare their employees against
+                        the rest of the industry using an interactive dashboard to upload, filter,
+                        and search all important information through one single platform.
+                    </p>
+                    <Button href="/login">Log in</Button>
+                </Column>
+                <Column lg={8} md={4} sm={4} /*className="landing-page__banner"*/>
+                    <img src={homeImage} alt="error" className="home-image"/>
+                </Column>
+            </Grid>
+            <Grid className="info">
+                <Column lg={4} md={4} sm={4} className="info-title">
+                    <h1>IBM CD</h1>
+                </Column>
+                <Column lg={4} md={4} sm={4} className="info-columnn">
+                    <h3>Upload</h3>
+                    <p className="info-body">
+                        Store your certification information in the cloud for other employees to access.
+                    </p>
+                    <Upload size={32} />
+                </Column>
+                <Column lg={4} md={4} sm={4} className="info-columnn">
+                    <h3>Filter</h3>
+                    <p className="info-body">
+                        Visualize and control employee data with an interactive dashboard and menu.
+                    </p>
+                    <Filter size={32} />
+                </Column>
+                <Column lg={4} md={4} sm={4} className="info-columnn">
+                    <h3>Search</h3>
+                    <p className="info-body">
+                        Receive personalized recommendations for each employee with artificial intelligence.
+                    </p>
+                    <Search size={32} />
+                </Column>
+            </Grid>
         </Content>
-        <Content>
-
-        </Content>
-
     </>
 )
 
