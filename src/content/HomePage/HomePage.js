9a7f3608b145faef39f3ee5f15db23a5d9a3ddb9
Fausto Palma <a01639224@tec.mx>
2023-05-23 21:15:52 -0600

    Updates to Home Page and Login Page

--- src/content/HomePage/HomePage.js
+++ src/content/HomePage/HomePage.js
@@ -1,69 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Theme, Header, HeaderName, HeaderGlobalBar, OverflowMenu, OverflowMenuItem} from '@carbon/react';
 import {Content, Grid, Column, Button} from '@carbon/react';
 import {User, Upload, Filter, Search} from '@carbon/react/icons';
 import homeImage from './homePageImage.jpg';
+import {Link} from "react-router-dom";
 
-const HomePage = () => (
-    <>
-        <Theme theme="g100">
-            <Header aria-label="IBM Platform Name">
-                <HeaderName prefix="IBM">
-                    Certifications Dashboard
-                </HeaderName>
-                <HeaderGlobalBar>
-                    <OverflowMenu
-                        flipped="true"
-                        renderIcon={() => <User size={20}/>}
-                        size="lg">
-                        <OverflowMenuItem itemText="Log in" href="/login" className="overflow-menu-item"/>
-                    </OverflowMenu>
-                </HeaderGlobalBar>
-            </Header>
-        </Theme>
-        <Content>
-            <Grid className="banner">
-                <Column lg={8} md={4} sm={4} className="header">
-                    <h1>Your one-stop certifications hub</h1>
-                    <p className="header-body">
-                        The IBM Certifications Dashboard allows IBM to compare their employees against
-                        the rest of the industry using an interactive dashboard to upload, filter,
-                        and search all important information through one single platform.
-                    </p>
-                    <Button href="/login">Log in</Button>
-                </Column>
-                <Column lg={8} md={4} sm={4} /*className="landing-page__banner"*/>
-                    <img src={homeImage} alt="error" className="home-image"/>
-                </Column>
-            </Grid>
-            <Grid className="info">
-                <Column lg={4} md={4} sm={4} className="info-title">
-                    <h1>IBM CD</h1>
-                </Column>
-                <Column lg={4} md={4} sm={4} className="info-columnn">
-                    <h3>Upload</h3>
-                    <p className="info-body">
-                        Store your certification information in the cloud for other employees to access.
-                    </p>
-                    <Upload size={32} />
-                </Column>
-                <Column lg={4} md={4} sm={4} className="info-columnn">
-                    <h3>Filter</h3>
-                    <p className="info-body">
-                        Visualize and control employee data with an interactive dashboard and menu.
-                    </p>
-                    <Filter size={32} />
-                </Column>
-                <Column lg={4} md={4} sm={4} className="info-columnn">
-                    <h3>Search</h3>
-                    <p className="info-body">
-                        Receive personalized recommendations for each employee with artificial intelligence.
-                    </p>
-                    <Search size={32} />
-                </Column>
-            </Grid>
-        </Content>
-    </>
-)
+const HomePage = () => {
+    const [pageDir, setPageDir] = useState('/login');
+
+    return (
+        <>
+            <Theme theme="g100">
+                <Header aria-label="IBM Platform Name">
+                    <HeaderName prefix="IBM" >
+                        Certifications Dashboard
+                    </HeaderName>
+                    <HeaderGlobalBar>
+                        <OverflowMenu
+                            flipped="true"
+                            renderIcon={() => <User size={20}/>}
+                            size="lg">
+                            <Link to={pageDir} className="overflow-menu-link">
+                                <OverflowMenuItem itemText="Log in" className="overflow-menu-item"/>
+                            </Link>
+                        </OverflowMenu>
+                    </HeaderGlobalBar>
+                </Header>
+            </Theme>
+            <Content>
+                <Grid className="banner">
+                    <Column lg={8} md={4} sm={4} className="header">
+                        <h1>Your one-stop certifications hub</h1>
+                        <p className="header-body">
+                            The IBM Certifications Dashboard allows IBM to compare their employees against
+                            the rest of the industry using an interactive dashboard to upload, filter,
+                            and search all important information through one single platform.
+                        </p>
+                        <Button href="/login">Log in</Button>
+                    </Column>
+                    <Column lg={8} md={4} sm={4}>
+                        <img src={homeImage} alt="Interior of a Summit node" className="home-image"/>
+                    </Column>
+                </Grid>
+                <Grid className="info">
+                    <Column lg={4} md={4} sm={4} className="info-title">
+                        <h1>IBM CD</h1>
+                    </Column>
+                    <Column lg={4} md={4} sm={4} className="info-columnn">
+                        <h3>Upload</h3>
+                        <p className="info-body">
+                            Store your certification information in the cloud for other employees to access.
+                        </p>
+                        <Upload size={32} />
+                    </Column>
+                    <Column lg={4} md={4} sm={4} className="info-columnn">
+                        <h3>Filter</h3>
+                        <p className="info-body">
+                            Visualize and control employee data with an interactive dashboard and menu.
+                        </p>
+                        <Filter size={32} />
+                    </Column>
+                    <Column lg={4} md={4} sm={4} className="info-columnn">
+                        <h3>Search</h3>
+                        <p className="info-body">
+                            Receive recommendations for each employee with artificial intelligence.
+                        </p>
+                        <Search size={32} />
+                    </Column>
+                </Grid>
+            </Content>
+        </>
+    )
+}
 
 export default HomePage;
--- src/content/LoginPage/LoginPage.js
+++ src/content/LoginPage/LoginPage.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Header, HeaderName, Theme } from '@carbon/react';
-import { Grid, Column, TextInput, Button } from '@carbon/react';
-import { Link } from 'react-router-dom';
-import { ArrowRight } from '@carbon/react/icons';
+import React, {useState} from 'react';
+import {Header, HeaderName, Theme} from '@carbon/react';
+import {Content,TextInput, Button} from '@carbon/react';
+import {ArrowRight} from '@carbon/react/icons';
+import {Link} from 'react-router-dom';
 
 var formData = new URLSearchParams();
 const emailRegex = /^[\w.%+-]+@tec\.mx$/i;
 
-const HomePage = () => {
+const LoginPage = () => {
     const saveTemporalPasswordRequestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -19,6 +19,7 @@ const HomePage = () => {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: formData
     }
+
     const [errorMessage, setErrorMessage] = useState('');
     const [pageDir, setPageDir] = useState('/login');
     const [twoFACode, setTwoFACode] = useState('');
@@ -28,7 +29,7 @@ const HomePage = () => {
 
     const handleLoginButtonClick = () => {
         if(pageDir === "/login") {
-            setTwoFAErrorMessage("Invalid 2FA code");
+            setTwoFAErrorMessage("Invalid One-Time Password");
         }
     };
 
@@ -68,12 +69,29 @@ const HomePage = () => {
                 })
                 .catch(error => {
                     console.error(error);
-                    setErrorMessage('Invalid email');
+                    setErrorMessage('Invalid IBM email address');
                 });
-            formData = new URLSearchParams();
-            setIsRequestingTwoFA(true);
+            // formData = new URLSearchParams();
+            // PIZARRO
+            // setIsRequestingTwoFA(true);
+            // Front-end changes once OTP is sent
+            var continueButton = document.getElementById("continue-button");
+            var resendButton = document.getElementById("resend-button");
+            var loginButton = document.getElementById("log-in-button");
+            var emailInput = document.getElementById("email-input");
+            var passwordInput = document.getElementById("password-input");
+            if(resendButton.style.display === "inline") {
+                resendButton.disabled = "true";
+            }
+            if(continueButton.style.display !== "none") {
+                // emailInput.style.visibility = "hidden";
+                continueButton.style.display = "none";
+                // passwordInput.style.display = "inline";
+                resendButton.style.display = "inline";
+                loginButton.style.display = "inline";
+            }
         }
-        setErrorMessage(isValidEmail ? '' : 'Invalid email');
+        setErrorMessage(isValidEmail ? '' : 'Invalid IBM email address');
     };
 
     const handleLogin = () => {
@@ -90,71 +108,112 @@ const HomePage = () => {
                 console.error('Error', error);
                 setErrorMessage("Internal server error");
             });
-        formData = new URLSearchParams();
+        // formData = new URLSearchParams();
     };
 
     return (
-        <body className="login-background">
-        <Theme theme="g100">
-            <Header aria-label="IBM Platform Name">
-                <HeaderName prefix="IBM">
-                    Certifications Dashboard
-                </HeaderName>
-            </Header>
-        </Theme>
-        <Grid fullWidth condensed>
-            <Column lg={16} md={8} sm={4} className="upper-break" />
-            <Column lg={16} md={8} sm={4}>
-                <Grid fullWidth condensed>
-                    <Column
-                        lg={{ span: 8, offset: 4 }}
-                        md={{ span: 4, offset: 2 }}
-                        sm={{ span: 2, offset: 1 }}
-                    >
-                        <Theme theme="g100">
-                            <h3>&nbsp;Login to the IBM Certifications Dashboard</h3>
-                            <TextInput
-                                id="text-input-1"
-                                type="email"
-                                labelText={<>&nbsp;&nbsp;IBMid</>}
-                                onChange={handleEmailInputChange}
-                                placeholder="username@ibm.com"
-                                invalid={errorMessage !== ''}
-                                invalidText={errorMessage}
+        <>
+            <Theme theme="g100">
+                <Header aria-label="IBM Platform Name">
+                    <HeaderName prefix="IBM">
+                        Certifications Dashboard
+                    </HeaderName>
+                </Header>
+            </Theme>
+            <Content className="login-background">
+                <div className="content-div">
+                    <Theme theme="g100">
+                        <h3 className="header-padding">Log in to the IBM Certifications Dashboard</h3>
+                        <TextInput
+                            id="email-input"
+                            type="email"
+                            labelText="w3id Credentials"
+                            onChange={handleEmailInputChange}
+                            placeholder="username@ibm.com"
+                            invalid={errorMessage !== ''}
+                            invalidText={errorMessage}
+                            className="email-input"
+                        />
+                        <div className="password-input">
+                            <TextInput.PasswordInput
+                                id="password-input"
+                                type="password"
+                                labelText="One-Time Password"
+                                onChange={handleTwoFACodeChange}
+                                placeholder="A six-digit OTP has been sent to your w3id email"
+                                invalid={twoFAErrorMessage !== ''}
+                                invalidText={twoFAErrorMessage}
+                                maxLength={6}
                             />
-                            {isRequestingTwoFA ? (
-                                <>
-                                    <Button enabled>
-                                        Request 2FA Code
-                                    </Button>
-                                    <div>&nbsp;</div>
-                                    <TextInput
-                                        id="text-input-2fa"
-                                        type="password"
-                                        labelText={<>&nbsp;&nbsp;2FA Code</>}
-                                        onChange={handleTwoFACodeChange}
-                                        invalid={twoFAErrorMessage !== ''}
-                                        invalidText={twoFAErrorMessage}
-                                        maxLength={6}
-                                    />
-                                </>
-                            ) : (
-                                <Button onClick={handleRequestTwoFA}>
-                                    Request 2FA Code
-                                </Button>
-                            )}
-                            <Link to={pageDir}>
-                                <Button onClick={handleLoginButtonClick} renderIcon={ArrowRight} disabled={twoFACode.length !== 6}>
-                                    Login
-                                </Button>
-                            </Link>
-                        </Theme>
-                    </Column>
-                </Grid>
-            </Column>
-        </Grid>
-        </body>
+                        </div>
+                        <Button onClick={handleRequestTwoFA} id="continue-button" renderIcon={ArrowRight} className="continue-button">
+                            Continue
+                        </Button>
+                        <Button onClick={handleRequestTwoFA} id="resend-button" className="resend-button" kind="secondary" >
+                            Resend OTP
+                        </Button>
+                        <Link to={pageDir} className="log-in-link">
+                            <Button onClick={handleLoginButtonClick} id="log-in-button" renderIcon={ArrowRight} className="log-in-button">
+                                Log in
+                            </Button>
+                        </Link>
+                    </Theme>
+                </div>
+            </Content>
+
+            {/*<Grid fullWidth condensed>*/}
+            {/*    <Column lg={16} md={8} sm={4} className="upper-break" />*/}
+            {/*    <Column lg={16} md={8} sm={4}>*/}
+            {/*        <Grid fullWidth condensed>*/}
+            {/*            <Column*/}
+            {/*                lg={{ span: 8, offset: 4 }}*/}
+            {/*                md={{ span: 4, offset: 2 }}*/}
+            {/*                sm={{ span: 2, offset: 1 }}*/}
+            {/*            >*/}
+            {/*                <Theme theme="g100">*/}
+            {/*                    <h3>&nbsp;Login to the IBM Certifications Dashboard</h3>*/}
+            {/*                    <TextInput*/}
+            {/*                        id="text-input-1"*/}
+            {/*                        type="email"*/}
+            {/*                        labelText={<>&nbsp;&nbsp;IBMid</>}*/}
+            {/*                        onChange={handleEmailInputChange}*/}
+            {/*                        placeholder="username@ibm.com"*/}
+            {/*                        invalid={errorMessage !== ''}*/}
+            {/*                        invalidText={errorMessage}*/}
+            {/*                    />*/}
+            {/*                    {isRequestingTwoFA ? (*/}
+            {/*                        <>*/}
+            {/*                            <Button enabled>*/}
+            {/*                                Request 2FA Code*/}
+            {/*                            </Button>*/}
+            {/*                            <div>&nbsp;</div>*/}
+            {/*                            <TextInput*/}
+            {/*                                id="text-input-2fa"*/}
+            {/*                                type="password"*/}
+            {/*                                labelText={<>&nbsp;&nbsp;2FA Code</>}*/}
+            {/*                                onChange={handleTwoFACodeChange}*/}
+            {/*                                invalid={twoFAErrorMessage !== ''}*/}
+            {/*                                invalidText={twoFAErrorMessage}*/}
+            {/*                                maxLength={6}*/}
+            {/*                            />*/}
+            {/*                        </>*/}
+            {/*                    ) : (*/}
+            {/*                        <Button onClick={handleRequestTwoFA}>*/}
+            {/*                            Request 2FA Code*/}
+            {/*                        </Button>*/}
+            {/*                    )}*/}
+            {/*                    <Link to={pageDir}>*/}
+            {/*                        <Button onClick={handleLoginButtonClick} renderIcon={ArrowRight} disabled={twoFACode.length !== 6}>*/}
+            {/*                            Login*/}
+            {/*                        </Button>*/}
+            {/*                    </Link>*/}
+            {/*                </Theme>*/}
+            {/*            </Column>*/}
+            {/*        </Grid>*/}
+            {/*    </Column>*/}
+            {/*</Grid>*/}
+        </>
     );
 };
 
-export default HomePage;
+export default LoginPage;
